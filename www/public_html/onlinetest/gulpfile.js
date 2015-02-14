fd17345df0c7aebb15f5e960e6936bbd264587f1
Hernawan Fa'iz Abdillah <abdillah96.bu@gmail.com>
2015-02-14 07:26:40 +0700

    Separate font, image, and library

--- www/public_html/onlinetest/gulpfile.js
+++ www/public_html/onlinetest/gulpfile.js
@@ -11,6 +11,8 @@ var devdir = '../../laravel4/app/dist/';
 var styledevdir = devdir + 'style/';
 var scriptdevdir = devdir + 'script/';
 var libdevdir = devdir + 'lib/';
+var fontdevdir = devdir + 'font/';
+var imgdevdir = devdir + 'img/';
 
 /**
  * Compile sass based style and put on temporary dir
@@ -18,7 +20,7 @@ var libdevdir = devdir + 'lib/';
 gulp.task('compile_sass', function() {
     return sass(styledevdir + 'raw/main.sass', ({ style: 'expanded' }))
         .pipe(rename({ extname: '.max.css' }))
-        .pipe(gulp.dest(styledevdir + 'temp'))
+        .pipe(gulp.dest(styledevdir + 'temp'));
 });
 
 /**
@@ -30,7 +32,7 @@ gulp.task('minify_css', ['compile_sass'], function() {
         .pipe(rename({ extname: '.min.css' }))
         .pipe(minifycss({ processImport: false }))
         .pipe(gulp.dest(styledevdir + 'temp/'));
-})
+});
 
 /**
  * Concatenate all style and put on public dir
@@ -40,16 +42,35 @@ gulp.task('styles', ['minify_css'], function() {
         .pipe(concat('styles.min.css'))
         .pipe(gulp.dest('./style'))
         .pipe(notify({ message: 'SASS compiled, all styles minifyed and concated.' }));
-})
+});
 
 /**
- * Concatenate all style and put on public dir
+ * Copy libs, scripts, fonts, images
  */
-gulp.task('libs', ['minify_css'], function() {
+gulp.task('libs', function() {
     return gulp.src(libdevdir + '**/*')
-        .pipe(gulp.dest('./lib'))
-        .pipe(notify({ message: 'Library copied.' }));
-})
+        .pipe(gulp.dest('./lib'));
+});
+
+gulp.task('fonts', function() {
+    return gulp.src(fontdevdir + '**/*')
+        .pipe(gulp.dest('./font'));
+});
+
+gulp.task('imgs', function() {
+    return gulp.src(imgdevdir + '**/*')
+        .pipe(gulp.dest('./img'));
+});
+
+gulp.task('htmls', function() {
+    return gulp.src(devdir + '**/*.html')
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('others', function() {
+    return gulp.src(devdir + '*')
+        .pipe(gulp.dest('./'));
+});
 
 /**
  * Minify (uglify) then concatenate all script and put on public dir
@@ -60,25 +81,32 @@ gulp.task('scripts', function() {
         .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('./script'))
         .pipe(notify({ message: 'JavaScript ready.' }));
-})
+});
 
 /**
  * Watch for change event
  */
 gulp.task('watch', function() {
-    gulp.watch(styledevdir + 'raw/*.sass', ['styles'])
-    gulp.watch(scriptdevdir + 'raw/*.js', ['scripts'])
-    gulp.watch(libdevdir + '**/*', ['libs'])
+    gulp.watch(styledevdir + 'raw/*.sass', ['styles']);
+    gulp.watch(scriptdevdir + 'raw/*.js', ['scripts']);
+    gulp.watch(libdevdir + '**/*', ['libs']);
+    gulp.watch(imgdevdir + '**/*', ['imgs']);
+    gulp.watch(devdir + '**/*.html', ['htmls']);
+    gulp.watch(devdir + '*', ['others']);
 
     // Buat server livereload
     livereload.listen({
-        port: 9000,
+        port: 9000
     });
     gulp.watch(['**/*']).on('change', livereload.changed);
     gulp.watch(['../../laravel4/app/views/**/*.blade.php']).on('change', livereload.changed);
-})
+});
 
 gulp.task('default', function() {
     gulp.start('styles');
     gulp.start('scripts');
+    gulp.start('libs');
+    gulp.start('imgs');
+    gulp.start('htmls');
+    gulp.start('others');
 });
