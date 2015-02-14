abc6ce8934b2028d4d19f6f9ca74518b78ebb777
Hernawan Fa'iz Abdillah <abdillah96.bu@gmail.com>
2015-02-15 01:06:21 +0700

    Add relationship to package and type

--- www/laravel4/app/models/Question.php
+++ www/laravel4/app/models/Question.php
@@ -3,12 +3,18 @@
 class Question extends Eloquent {
 
 
-  /**
-   * The database table used by the model.
-   *
-   * @var string
-   */
-  protected $table = 'questions';
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'questions';
 
+    public function qtype() {
+        return $this->belongsTo('QType');
+    }
 
+    public function qpackages() {
+        return $this->belongsToMany('QPackage');
+    }
 }
