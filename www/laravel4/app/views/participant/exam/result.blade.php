330899e1b6d974ef38f7b044cd5e57136fddee08
Hernawan Fa'iz Abdillah <abdillah96.bu@gmail.com>
2015-02-23 09:28:45 +0700

    Construct score view

--- www/laravel4/app/views/participant/exam/result.blade.php
+++ www/laravel4/app/views/participant/exam/result.blade.php
@@ -4,8 +4,46 @@
 
   <main class="container-fluid">
     <h1>Hasil Ujian</h1>
-    <p>Tim {{ $participant->team_name }}</p>
-    <p>Skor : {{ $exam->score }}</p>
+	<hr/>
+
+	<section class="score-board">
+	  <div class="col-sm-6">
+		<h2>Perhitungan</h2>
+		<div class="col-sm-3">Benar</div>
+		<div class="col-sm-2"> : </div>
+		<div class="col-sm-4" style="font-family: monospace">({{ $exam->score_true }} x 4)</div>
+		<div class="col-sm-2">{{ $exam->score_true * 4 }}</div>
+
+		<div class="col-sm-3">Salah</div>
+		<div class="col-sm-2"> : </div>
+		<div class="col-sm-4" style="font-family: monospace">({{ $exam->score_false }} x -1)</div>
+		<div class="col-sm-2">{{ $exam->score_false * -1 }}</div>
+
+		<div class="col-sm-3">Kosong</div>
+		<div class="col-sm-2"> : </div>
+		<div class="col-sm-4" style="font-family: monospace">({{ $exam->score_null }} x 0)</div>
+		<div class="col-sm-2">{{ $exam->score_null * 0 }}</div>
+
+		<hr class="col-sm-10"/>
+
+		<div class="col-sm-3">Total</div>
+		<div class="col-sm-6"> : </div>
+		<div class="col-sm-2"><strong>{{ $exam->score }}</strong></div>
+	  </div>
+
+	  <div class="col-sm-6">
+		<h2>Point Akhir</h2>
+		<p>Tim kamu, "{{ $participant->team_name }}" medapatkan skor : </p>
+		<h1><strong>{{ $exam->score }}</strong></h1>
+	  </div>
+	</section>
+
+	<div style="clear: both"></div>
+
+	<hr/>
+    <div class="paper-footer col-sm-12">
+      <small>(c) Technocorner</small>
+    </div>
   </main>
 
 @stop
