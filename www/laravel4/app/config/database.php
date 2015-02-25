150e6e3e90b3ece899712828ae5cb92cb63ca28c
Adhika Setya <adhika.setyap@gmail.com>
2015-02-25 21:50:28 +0700

    Set Production Server MYSQL config

--- www/laravel4/app/config/database.php
+++ www/laravel4/app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'forge',
-			'username'  => 'root',
-			'password'  => 'denita',
+			'database'  => 'technoco_eec_db',
+			'username'  => 'technoco_eec',
+			'password'  => 'UeY9LjyLYWn5',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
